3e1b7d111aab21fbbb9400c6603d1265a7442203
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-06-17 11:07:10 +0330

    Add new database connection

--- config/database.php
+++ config/database.php
@@ -82,7 +82,7 @@ return [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
+            'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE_YADAK', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD_YADAK', ''),
